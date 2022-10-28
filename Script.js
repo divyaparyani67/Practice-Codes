b670b072f8b3e8075b9aec515fa395d2783b126e
divyaparyani67 <divyaparyani67@gmail.com>
2022-10-28 14:18:11 +0530

    added basic files

---
+++ Script.js
@@ -0,0 +1,31 @@
+// splice
+// sort
+// pop
+//push
+// basic functions 
+// This KeyWord 
+// Jquery 
+// basic ajax Disadvantages and advantages (code writing)
+// random function  
+// strict  
+// arrays in js
+// Https request in ajax Status in ajax HTML - 
+// Testing in Javascript 
+// Functions 
+// Understanding of Asycnhronous behaviour 
+// Understanding of Ajax - 
+// fetch 
+// async and await
+// if else
+// regular expressions
+//objcets
+//pure and impure functions
+//concat
+//data types
+//constructor 
+// eventloop
+//iterators and generators
+//arrow function
+//differnce between map and fot each
+//api fetch
+modelwindow
