b0a54356102e708097810f8ff1f2d9e2136dd331
divyaparyani67 <divyaparyani67@gmail.com>
2023-04-12 22:31:05 +0530

    added notes

---
+++ test_practice/Practice.js
@@ -0,0 +1,74 @@
+//for (var i = 0; i < 5; i++) {
+//console.log(i);
+//}
+
+// var a = ["dog", "cat", "hen"];
+// a[100] = "fox";
+// console.log(a.length);
+
+// Boolean(0);
+// Boolean();
+
+
+// class X {
+//     get Y() {
+//       return 42;
+//     }
+//   }
+//   var x = new X();
+
+  //console.log(x.Y)
+
+// sum(10, 20);
+// diff(10, 20);
+// function sum(x, y) {
+//   return x + y;
+// }
+
+// let diff = function (x, y) {
+//   return x - y;
+// }
+
+// [1, 2] == [1, 2];
+
+
+// var a;
+// var b = (a = 3) ? true : false;
+
+// var start = 1;
+// function setEnd() {
+//   var end = 10;
+  
+// }
+// setEnd();
+
+// console.log(end);
+
+
+// function sayHello() {
+//     console.log('hello');
+//   }
+  
+//   console.log(sayHello.prototype);
+
+
+// function printA() {
+//     console.log(answer);
+//     var answer = 1;
+//   }
+//   printA();
+
+//   printA();
+
+//   function makeAdder(x) {
+//     return function (y) {
+//       return x + y;
+//     };
+//   }
+  
+//   var addFive = makeAdder(5);
+//   console.log(addFive(3));
+
+let rainForests = ['Amazon', 'Borneo', 'Cerrado', 'Congo'];
+rainForests.splice(0, 2);
+console.log(rainForests);
